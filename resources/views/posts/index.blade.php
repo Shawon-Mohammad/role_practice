dfa09c74b7c5039b15ab846df88af4f3f86f6f3c
Abdul Motaleb Shawon <abdulmotalebshawon@gmail.com>
2023-10-11 16:09:36 +0600

    add relation

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -80,7 +80,7 @@
                                         <td>{{ $post->id }}</td>
                                         <td>{{ $post->title }}</td>
                                         <td>{{ $post->body }}</td>
-                                        <td>{{ $post->user_id }}</td>
+                                        <td>{{ $post->user->name }}</td>
                                         <td>{{ $post->status }}</td>
                                         <td>{{ $post->created_at }}</td>
                                         <td>{{ $post->updated_at }}</td>
